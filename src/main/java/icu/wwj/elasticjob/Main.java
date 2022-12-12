83a9c5c6a20dcf337fa7b6480eb18369acfeaa34
吴伟杰 <wuweijie@apache.org>
2022-12-12 23:42:27 +0800

    New Project

---
+++ src/main/java/icu/wwj/elasticjob/Main.java
@@ -0,0 +1,11 @@
+package icu.wwj.elasticjob;
+
+import io.javaoperatorsdk.operator.Operator;
+
+public class Main {
+    public static void main(String[] args) {
+        Operator operator = new Operator();
+//        operator.register();
+        operator.start();
+    }
+}
